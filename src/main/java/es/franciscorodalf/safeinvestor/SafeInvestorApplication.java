9f51a4306f4db7b5a935e60b1884d21cae826f95
Francisco Rodriguez Alfonso <franteleco1278@gmail.com>
2026-06-01 18:51:57 +0100

    feat: añadir punto de entrada de la aplicación Spring Boot

---
+++ src/main/java/es/franciscorodalf/safeinvestor/SafeInvestorApplication.java
@@ -0,0 +1,12 @@
+package es.franciscorodalf.safeinvestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeInvestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeInvestorApplication.class, args);
+    }
+}
